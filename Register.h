78a558f520b76e85f8c042f472c8879ec185eaaa
Gamerface21 <Gamerface21@users.noreply.github.com>
2017-09-14 14:43:38 -0400

    Register.h

---
+++ Register.h
@@ -0,0 +1,20 @@
+
+//Class definition
+//Register.h
+#ifndef REGISTER_H
+#define REGISTER_H
+
+class Register
+{
+	private:
+		int age=0;
+		string name=" ";
+	
+	public:
+		Register(string n, int a)
+		string getname();
+		void setname(string);
+		int getage();
+		void setage(int);
+}
+#endif
